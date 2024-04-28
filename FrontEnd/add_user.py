c33469340fdf499e36726aa7b893b685730f9005
Mohamed Fadel <116431641+Mohamed-Fadel222@users.noreply.github.com>
2024-04-28 23:33:12 +0300

    Update add_user.py

--- FrontEnd/add_user.py
+++ FrontEnd/add_user.py
@@ -9,10 +9,6 @@ class AddUserPage(tk.Frame):
         self.db_connection = get_shared_connection()
 
         # Labels and Entry fields for user details
-        tk.Label(self, text="ID:").grid(row=0, column=0, padx=20, pady=10, sticky="e")
-        self.id_entry = tk.Entry(self)
-        self.id_entry.grid(row=0, column=1, padx=20, pady=10, sticky="w")
-
         tk.Label(self, text="Name:").grid(row=1, column=0, padx=20, pady=10, sticky="e")
         self.name_entry = tk.Entry(self)
         self.name_entry.grid(row=1, column=1, padx=20, pady=10, sticky="w")
@@ -41,13 +37,16 @@ class AddUserPage(tk.Frame):
         self.city_entry = tk.Entry(self)
         self.city_entry.grid(row=7, column=1, padx=20, pady=10, sticky="w")
 
+        tk.Label(self, text="Phone Number:").grid(row=8, column=0, padx=20, pady=10, sticky="e")
+        self.phone_entry = tk.Entry(self)
+        self.phone_entry.grid(row=8, column=1, padx=20, pady=10, sticky="w")
+
         # Button to add user
-        tk.Button(self, text="Add User", command=self.add_user).grid(row=8, columnspan=2, padx=20, pady=20)
+        tk.Button(self, text="Add User", command=self.add_user).grid(row=9, columnspan=2, padx=20, pady=20)
         
-        tk.Button(self, text="Home", command=self.go_to_homepage).grid(row=9, columnspan=2, padx=20, pady=20)
+        tk.Button(self, text="Home", command=self.go_to_homepage).grid(row=10, columnspan=2, padx=20, pady=20)
 
     def add_user(self):
-        member_id = self.id_entry.get()
         name = self.name_entry.get()
         email = self.email_entry.get()
         password = self.password_entry.get()
@@ -55,19 +54,19 @@ class AddUserPage(tk.Frame):
         street = self.street_entry.get()
         building = self.building_entry.get()
         city = self.city_entry.get()
+        phone_number = self.phone_entry.get()
 
-        if not member_id or not name or not email or not password:
-            messagebox.showerror("Error", "Name, email, and password are required fields")
+        if not name or not email or not password or not phone_number:
+            messagebox.showerror("Error", "Name, email, password, and phone number are required fields")
             return
 
         try:
             cursor = self.db_connection.cursor()
-            cursor.execute("INSERT INTO Member (ID, Name, email, password, Gender, Street_name, Building_number, City) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-                           (member_id, name, email, password, gender, street, building, city))
+            cursor.execute("EXEC add_user_procedure ?, ?, ?, ?, ?, ?, ?, ?",
+                           (name, email, password, gender, street, building, city, phone_number))
             self.db_connection.commit()
             messagebox.showinfo("Success", "User added successfully")
             # Clear the entry fields after adding user
-            self.id_entry.delete(0, tk.END)
             self.name_entry.delete(0, tk.END)
             self.email_entry.delete(0, tk.END)
             self.password_entry.delete(0, tk.END)
@@ -75,6 +74,7 @@ class AddUserPage(tk.Frame):
             self.street_entry.delete(0, tk.END)
             self.building_entry.delete(0, tk.END)
             self.city_entry.delete(0, tk.END)
+            self.phone_entry.delete(0, tk.END)
         except Exception as e:
             messagebox.showerror("Error", str(e))
         finally:
