8f58ec34d2b68a84b0ce5804c886283ded340113
Kareem Ahmed <120688051+kareem2002-k@users.noreply.github.com>
2024-04-29 13:09:42 +0300

    updatre

--- FrontEnd/add_genre.py
+++ FrontEnd/add_genre.py
@@ -17,9 +17,6 @@ class AddGenre(tk.Frame):
         self.name_entry = tk.Entry(self)
         self.name_entry.grid(row=1, column=1, padx=20, pady=10, sticky="w")
 
-        tk.Label(self, text="Number of Books:").grid(row=2, column=0, padx=20, pady=10, sticky="e")
-        self.num_books_entry = tk.Entry(self)
-        self.num_books_entry.grid(row=2, column=1, padx=20, pady=10, sticky="w")
 
         # Button to add genre
         tk.Button(self, text="Add Genre", command=self.add_genre).grid(row=3, columnspan=2, padx=20, pady=20)
@@ -29,22 +26,18 @@ class AddGenre(tk.Frame):
     def add_genre(self):
         genre_id = self.id_entry.get()
         name = self.name_entry.get()
-        num_books = self.num_books_entry.get()
 
-        if not genre_id or not name or not num_books:
-            messagebox.showerror("Error", "ID, Name, and Number of Books are required fields")
-            return
+       
 
         try:
             cursor = self.db_connection.cursor()
-            cursor.execute("INSERT INTO Genre (Genre_ID, NAME, Number_of_books) VALUES (?, ?, ?)",
-                           (genre_id, name, num_books))
+            cursor.execute("INSERT INTO Genre (Genre_ID, NAME, Number_of_books) VALUES (?, ?, 0)",
+                           (genre_id, name))
             self.db_connection.commit()
             messagebox.showinfo("Success", "Genre added successfully")
             # Clear the entry fields after adding genre
             self.id_entry.delete(0, tk.END)
             self.name_entry.delete(0, tk.END)
-            self.num_books_entry.delete(0, tk.END)
         except Exception as e:
             messagebox.showerror("Error", str(e))
         finally:
